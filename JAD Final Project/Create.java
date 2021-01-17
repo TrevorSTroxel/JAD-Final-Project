36daad7ee15edeee782cca76b19baa6ed3a63688
TrevorSTroxel <ttroxel21@my.whitworth.edu>
2021-01-17 13:16:44 -0800

    Added the starting functionality of out program

--- JAD Final Project/Create.java
+++ JAD Final Project/Create.java
@@ -1,15 +1,52 @@
 // Creation file: contains method necessary to create instnaces for the program
+#import java.io.File;
 public class Create 
 {
-	
-	// ) (   ) (   ) (   ) (   ) (   ) (   ) (   ) (   ) (   ) (   ) (
+	/**
+	 * @param location
+	 * @return String
+	 * What this function will do is call the appropiate methods
+	 *                 that are needed depending on the input It will first look at
+	 *                 what the user want to do, then it will grab the whole string
+	 *                 input, split it up and then look at the naming convention and
+	 *                 other things like that
+	 */
+
+	String[] WordSplit; //this a generic variable that will be called when we need to slit the users input
+	public void Creation(String methodCall, String CompleteString) {
+
+	}
+
+	// ) ( ) ( ) ( ) ( ) ( ) ( ) ( ) ( ) ( ) ( ) (
 	// Class constructor
-	//	CREATE A CLASS
-	//	Function Input: string |  Function Output:a class in string form
-	// ) (   ) (   ) (   ) (  
-	public String creatr_clas(String input) 
+	// CREATE A CLASS
+	// Function Input: string: name of class | Function Output:a class in string
+	// form
+	// ) ( ) ( ) ( ) (
+	public String creatr_class(String class_name) {
+		return String.format("public class %s{}", class_name);
+	}
+
+	// ) ( ) ( ) ( ) ( ) ( ) ( ) ( ) ( ) ( ) ( ) (
+	// Method contructor
+	// CREATE A METHOD
+	// Function Input: string: a return type and a name for the method | Function
+	// Output:a method in string form
+	// ) ( ) ( ) ( ) (
+	public String create_method(String return_type, String method_name) {
+
+		return String.format("public %s %s(){}", return_type, method_name);
+	}
+
+	// ) ( ) ( ) ( ) ( ) ( ) ( ) ( ) ( ) ( ) ( ) (
+	// File contructor
+	// CREATE A FILE
+	// Function Input: string | Function Output:a file in chosen directory
+	// ) ( ) ( ) ( ) (
+	public void create_file(String file_name, String file_directory) 
 	{
-		String cls = "";
-		return cls;
+		File new_file = new File(file_directory);
+		new_file.getParentFile().mkdirs(); 
+		new_file.createNewFile();
 	}
 }
--- JAD Final Project/Window.java
+++ JAD Final Project/Window.java
@@ -9,9 +9,14 @@ public class Window extends JFrame implements ActionListener {
      */
     private static final long serialVersionUID = 1L;
 
+    Create creation = new Create();
+
     JFrame MainWindow = new JFrame("Jave Program Creation"); // creates the main window for the user to see
     JPanel TextInput = new JPanel(); // this will hold the text input the user will do to call the different classes
     JTextField userInput = new JTextField(); // this will be where the user types what they want to do
+    JButton Enter = new JButton("Enter"); // this will be used for determining what file to go to and what method to
+                                          // call
+    String[] WordChoice;
 
     // we created this method to call so that if we want to add more we can just add
     // it here rather than adding on to methods that already have a lot of stuff in
@@ -26,15 +31,28 @@ public class Window extends JFrame implements ActionListener {
     private void addContents() {
         MainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        TextInput.add(Enter);
+        userInput.setEditable(true); // this makes it so that the user can edit what they put in the text field
         TextInput.setEnabled(true);
         MainWindow.add(TextInput);
         MainWindow.setVisible(true);
 
     }
 
+    /**
+     * @param e
+     * @apiNote this will look at the first word that the user has writen and the
+     *          program will go to the correct methods as needed
+     */
+
     @Override
     public void actionPerformed(ActionEvent e) {
-        // TODO Auto-generated method stub
+        String INPUT = userInput.getText();
+        WordChoice = INPUT.split(" "); // this will split up the input that the user has made and from there the
+                                       // program will go to the correct file to run the methods
+        if (e.getSource() == Enter && WordChoice[0] == "Create" || WordChoice[0] == "create") {
+            creation.Creation(WordChoice[1], INPUT);
+        }
 
     }
 
