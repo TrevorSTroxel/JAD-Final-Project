31c6182d08bd6e2a7503fc97b188acd204cfc338
Trevor Troxel <ttroxel21@my.whitworth.edu>
2021-01-17 12:32:47 -0800

    Add files via upload

---
+++ JAD Final Project/Create.java
@@ -0,0 +1,12 @@
+// Creation file: contains method necessary to create instnaces for the program
+public class Create 
+{
+	/*
+	// ) (   ) (   ) (   ) (   ) (   ) (   ) (   ) (   ) (   ) (   ) (
+	// Class constructor
+	//	
+	//	Function Input: NONE  |  Function Output: A BLANK AWARE PERSON
+	// ) (   ) (   ) (   ) (  
+
+	*/
+}
---
+++ JAD Final Project/Window.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Window extends JFrame {
+
+    /**
+     * this line is meant to keep VSCode happy
+     */
+    private static final long serialVersionUID = 1L;
+
+    JFrame MainWindow = new JFrame("Jave Program Creation"); // creates the main window for the user to see
+    JPanel TextInput = new JPanel(); // this will hold the text input the user will do to call the different classes
+
+}
