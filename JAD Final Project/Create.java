2bc7f3f0bdfe945a83986001dc186ea8d2eb6e0b
TrevorSTroxel <ttroxel21@my.whitworth.edu>
2021-01-17 12:47:54 -0800

    Co-authored-by: Abdallah-Abualkheir <Abdallah-Abualkheir@users.noreply.github.com>

--- JAD Final Project/Create.java
+++ JAD Final Project/Create.java
@@ -1,12 +1,15 @@
 // Creation file: contains method necessary to create instnaces for the program
 public class Create 
 {
-	/*
+	
 	// ) (   ) (   ) (   ) (   ) (   ) (   ) (   ) (   ) (   ) (   ) (
 	// Class constructor
-	//	
-	//	Function Input: NONE  |  Function Output: A BLANK AWARE PERSON
+	//	CREATE A CLASS
+	//	Function Input: string |  Function Output:a class in string form
 	// ) (   ) (   ) (   ) (  
-
-	*/
+	public String creatr_clas(String input) 
+	{
+		String cls = "";
+		return cls;
+	}
 }
--- JAD Final Project/Window.java
+++ JAD Final Project/Window.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class Window extends JFrame {
+public class Window extends JFrame implements ActionListener {
 
     /**
      * this line is meant to keep VSCode happy
@@ -11,5 +11,31 @@ public class Window extends JFrame {
 
     JFrame MainWindow = new JFrame("Jave Program Creation"); // creates the main window for the user to see
     JPanel TextInput = new JPanel(); // this will hold the text input the user will do to call the different classes
+    JTextField userInput = new JTextField(); // this will be where the user types what they want to do
+
+    // we created this method to call so that if we want to add more we can just add
+    // it here rather than adding on to methods that already have a lot of stuff in
+    // them
+    public void WindowCreation() {
+        addContents();
+    }
+
+    /**
+     * This will add most of the content to the main window function
+     */
+    private void addContents() {
+        MainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        TextInput.setEnabled(true);
+        MainWindow.add(TextInput);
+        MainWindow.setVisible(true);
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+
+    }
 
 }
