c5bb83db16143fe82c806bcd22889a82776a1035
TrevorSTroxel <ttroxel21@my.whitworth.edu>
2021-01-21 12:52:36 -0800

    Cleaned up code and addeed new method

--- JAD Final Project/Create.java
+++ JAD Final Project/Create.java
@@ -1,60 +1,34 @@
+
 //resources "https://www.w3schools.com/java/java_files_create.asp" reference to create and write to files
 // Creation file: contains method necessary to create instnaces for the program
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.FileWriter;   // Import the FileWriter class
+import java.io.FileWriter; // Import the FileWriter class
 import java.nio.file.FileAlreadyExistsException;
 
 public class Create {
-	/**
-	 * @param location
-	 * @return String What this function will do is call the appropiate methods that
-	 *         are needed depending on the input It will first look at what the user
-	 *         want to do, then it will grab the whole string input, split it up and
-	 *         then look at the naming convention and other things like that
-	 */
-
-	String[] WordSplit; // this a generic variable that will be called when we need to slit the users
-						// input
-	/*
-	// Leaving this in here until we know for sure we do not need this
-	// public void Creation(String CompleteString) {
-	// WordSplit = CompleteString.split(" "); // this will split up the string every
-	// time there is a space
-	// // depending on what the user decides to make, it will call different methods
-	// // WordSplit[1] = file : create_file
-	// if (WordSplit[1].equals("file")) {
-	// Window.Panel1.setVisible(false);
-	// //String dir = newInput.FileLocation();
-	// //String name = newInput.NameFile();
-	// //create_file(dir, name);
-	// } else if (WordSplit[1].equals("class")) {
-
-	// } else if (WordSplit[1].equals("method")) {
-
-	// }
-	// }
-	*/
 	// ) ( ) ( ) ( ) ( ) ( ) ( ) ( ) ( ) ( ) ( ) (
 	// Class constructor
 	// CREATE A CLASS
 	// Function Input: string: name of class | Function Output:a class in string
 	// form
 	// ) ( ) ( ) ( ) (
-	public static void creatr_class(String file_path, String class_name) 
-	{
-		try 
-		{
+	/**
+	 * @param file_path
+	 * @param class_name
+	 */
+	public static void creatr_class(String file_path, String class_name) {
+		try {
 			FileWriter myWriter = new FileWriter(file_path);
-			myWriter.write(String.format("public class %s{}", class_name));
+			myWriter.write(String.format("public class %s{\n\n}", class_name));
 			myWriter.close();
 			System.out.println("Successfully wrote to the file.");
-		  	} 
-			catch (IOException e) 
-			{
-				System.out.println("An error occurred.");
-				e.printStackTrace();
-			}
+		} catch (IOException e) {
+			System.out.println("An error occurred.");
+			e.printStackTrace();
+		}
 	}
 
 	// ) ( ) ( ) ( ) ( ) ( ) ( ) ( ) ( ) ( ) ( ) (
@@ -64,29 +38,33 @@ public class Create {
 	// and the file path they want to get
 	// Output:a method in string form
 	// ) ( ) ( ) ( ) (
-	public static void create_method(String File_Directory, String return_type, String method_name) 
-	{
+	/**
+	 * @param File_Directory
+	 * @param return_type
+	 * @param method_name
+	 */
+	public static void create_method(String File_Directory, String return_type, String method_name) {
 
-		try 
-		{
+		try {
 			FileWriter myWriter = new FileWriter(File_Directory);
-			myWriter.write(String.format("public %s %s(){}", return_type, method_name));
+			myWriter.write(String.format("public %s %s(){\n\n}", return_type, method_name));
 			myWriter.close();
 			System.out.println("Successfully wrote to the file.");
-		  	} 
-			catch (IOException e) 
-			{
-				System.out.println("An error occurred.");
-				e.printStackTrace();
-			}
-		
+		} catch (IOException e) {
+			System.out.println("An error occurred.");
+			e.printStackTrace();
+		}
+
 	}
 
-	// ) ( ) ( ) ( ) ( ) ( ) ( ) ( ) ( ) ( ) ( ) (
-	// File contructor
-	// CREATE A FILE
-	// Function Input: string | Function Output:a file in chosen directory
-	// ) ( ) ( ) ( ) (
+	/**
+	 * // ) ( ) ( ) ( ) ( ) ( ) ( ) ( ) ( ) ( ) ( ) ( // File contructor // CREATE A
+	 * FILE // Function Input: string | Function Output:a file in chosen directory
+	 * // ) ( ) ( ) ( ) (
+	 * 
+	 * @param file_directory
+	 * @param file_name
+	 */
 	public static void create_file(String file_directory, String file_name) {
 		try {
 			File new_file = new File(file_directory + "\\" + file_name);
@@ -98,5 +76,39 @@ public class Create {
 					.println("There was an error when creating your new file, try making sure you have correct inputs");
 			e.printStackTrace();
 		}
+	}
+
+	/**
+	 * @param Method_Name
+	 * @param File_dir
+	 * @param Contents_To_Add This method will search through the given file the
+	 *                        user has selected and look for the method the user has
+	 *                        asked for.
+	 * 
+	 *                        After this the program will insert contents they want
+	 *                        to add to the method they have called Reference:
+	 *                        https://www.coderslexicon.com/search-a-text-file-in-java/
+	 */
+	public static void Add_To_Method(String File_Dir, String Method_Name, String Contents_To_Add) {
+		File dir = new File(File_Dir);// this will open up the file directory that the user has chosen
+
+		try {
+			FileWriter content = new FileWriter(File_Dir);// this will enable us to write content to the file
+			BufferedReader FileReader = new BufferedReader(new FileReader(dir));
+			String line = "";
+			while ((line = FileReader.readLine()) != null) // this makes sure that the program does not read beyond the
+															// program
+			{
+				if (line.contains(Method_Name)) // this means that the program has found the method name
+				{
+					// this should now go to the next empty line and add contents
+				}
+			}
+			content.close();
+			FileReader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 	}
 }
