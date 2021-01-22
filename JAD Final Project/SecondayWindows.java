6481df216f3d023d2c124e0cf68d1a3507013bd6
TrevorSTroxel <ttroxel21@my.whitworth.edu>
2021-01-22 13:30:24 -0800

    Added methods for better user feedback

--- JAD Final Project/SecondayWindows.java
+++ JAD Final Project/SecondayWindows.java
@@ -1,11 +1,14 @@
+import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.filechooser.FileSystemView;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+//TODO: feedback on buttons for creating methods/classes/files
 //this will be used for better organization purposes
 public class SecondayWindows extends JFrame implements ActionListener {
 
@@ -47,79 +50,92 @@ public class SecondayWindows extends JFrame implements ActionListener {
         SecondWindow.setVisible(true);
     }
 
+    public static void DirButton(JButton JB) {
+        JB.setText("You have chosen your directory. Click again to change directories");
+    }
+
+    public static void nm_Button(JButton _JB) {
+        _JB.setText("Name has been set. Change file name for new file and click again");
+    }
+
+    public static void CreationButton(JButton __JB) {
+        __JB.setText(
+                "You have added to your project. Check your folder to make sure. Click again to add more to your project");
+    }
+
+    public static void FileSelect() {
+        JFileChooser j = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+        j.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        int r = j.showOpenDialog(null);
+        if (r == JFileChooser.APPROVE_OPTION) {
+            // set the label to the path of the selected directory
+            l.setText(j.getSelectedFile().getAbsolutePath());
+        }
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == WindowVar.P2_Enter_Get_Dir) {
-            JFileChooser j = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
-            j.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-            int r = j.showOpenDialog(null);
-            if (r == JFileChooser.APPROVE_OPTION) {
-                // set the label to the path of the selected directory
-                l.setText(j.getSelectedFile().getAbsolutePath());
-            }
+            FileSelect();
             WindowVar.P2_Dir_Text_Field.setText("Your directory is: " + l.getText());
+            DirButton(WindowVar.P2_Enter_Get_Dir);
             Panel2_DIR = l.getText();// this takes the name of the file directory that the user inputs
         } else if (e.getSource() == WindowVar.P2_Enter_Get_File_Name) {
             WordChoice = WindowVar.P2_File_Name.getText().split(" ");
+            nm_Button(WindowVar.P2_Enter_Get_File_Name);
             Panel2_NAME = WordChoice[8]; // this takes the name of the file directory that the user inputs
         } else if (e.getSource() == WindowVar.P2_Enter_Call_Method) {
+            CreationButton(WindowVar.P2_Enter_Call_Method);
             Create.create_file(Panel2_DIR, Panel2_NAME); // creates a new file in this location
         }
         /////////////////////////////////////////////////////////////////////////////////////
         else if (e.getSource() == WindowVar.P3_Enter_Get_Dir) {
-            JFileChooser j = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
-            j.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
-            int r = j.showOpenDialog(null);
-            if (r == JFileChooser.APPROVE_OPTION) {
-                // set the label to the path of the selected directory
-                l.setText(j.getSelectedFile().getAbsolutePath());
-            }
-            WindowVar.P2_File_Name.setText("Your directory is: " + l.getText());
+            FileSelect();
+            WindowVar.P3_File_Path.setText("Your directory is: " + l.getText());
+            DirButton(WindowVar.P3_Enter_Get_Dir);
             Panel3_FILEPATH = l.getText();// this takes the name of the file directory that the user inputs
         } else if (e.getSource() == WindowVar.P3_Enter_Get_Class_Name) {
             WordChoice = WindowVar.P3_Class_Name.getText().split(" ");
+            nm_Button(WindowVar.P3_Enter_Get_Class_Name);
             Panel3_CLASSNAME = WordChoice[8];
         } else if (e.getSource() == WindowVar.P3_Enter_Call_Method) {
+            CreationButton(WindowVar.P3_Enter_Call_Method);
             Create.creatr_class(Panel3_FILEPATH, Panel3_CLASSNAME);
         }
         /////////////////////////////////////////////////////////////////////////////////////
         else if (e.getSource() == WindowVar.P4_Enter_File_Dir) {
-            JFileChooser j = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
-            j.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
-            int r = j.showOpenDialog(null);
-            if (r == JFileChooser.APPROVE_OPTION) {
-                // set the label to the path of the selected directory
-                l.setText(j.getSelectedFile().getAbsolutePath());
-            }
+            FileSelect();
             WindowVar.P4_File_Path.setText("Your directory is: " + l.getText());
+            DirButton(WindowVar.P4_Enter_File_Dir);
             Panel4_File_Dir = l.getText();// this takes the name of the file directory that the user inputs
         } else if (e.getSource() == WindowVar.P4_Enter_Return_Type) {
             WordChoice = WindowVar.P4_Return_Type.getText().split(" ");
+            nm_Button(WindowVar.P4_Enter_Return_Type);
             Panel4_Return_Type = WordChoice[8];
         } else if (e.getSource() == WindowVar.P4_Enter_Method_Name) {
             WordChoice = WindowVar.P4_Method_Name.getText().split(" ");
+            nm_Button(WindowVar.P4_Enter_Method_Name);
             Panel4_Mathed_Name = WordChoice[6];
         } else if (e.getSource() == WindowVar.P4_Enter_Call_Method) {
+            CreationButton(WindowVar.P4_Enter_Call_Method);
             Create.create_method(Panel4_File_Dir, Panel4_Return_Type, Panel4_Mathed_Name);
         }
         /////////////////////////////////////////////////////////////////////////////////////
         else if (e.getSource() == WindowVar.P5_Enter_File_Dir) {
-            JFileChooser j = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
-            j.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
-            int r = j.showOpenDialog(null);
-            if (r == JFileChooser.APPROVE_OPTION) {
-                // set the label to the path of the selected directory
-                l.setText(j.getSelectedFile().getAbsolutePath());
-            }
+            FileSelect();
             WindowVar.P2_Dir_Text_Field.setText("Your directory is: " + l.getText());
+            DirButton(WindowVar.P5_Enter_File_Dir);
             Panel5_FileDir = l.getText();// this takes the name of the file directory that the user inputs
         } else if (e.getSource() == WindowVar.P5_Enter_Method_Name) {
             WordChoice = WindowVar.P5_Method_Name.getText().split(" ");
+            nm_Button(WindowVar.P5_Enter_Method_Name);
             Panel5_MethodName = WordChoice[6];
         } else if (e.getSource() == WindowVar.P5_Enter_Contents) {
             WordChoice = WindowVar.P5_Contents_To_Add.getText().split(" ");
+            nm_Button(WindowVar.P5_Enter_Contents);
             Panel5_Contents = WordChoice[9];
         } else if (e.getSource() == WindowVar.P5_Enter_Call_Method) {
+            CreationButton(WindowVar.P5_Enter_Call_Method);
             Create.Add_To_Method(Panel5_FileDir, Panel5_MethodName, Panel5_Contents);
         } else {
             System.out.println("Wrong command, try agian.");
