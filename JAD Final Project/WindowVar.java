32d501d8567d0fb8b634276136df402939a1860a
TrevorSTroxel <ttroxel21@my.whitworth.edu>
2021-01-21 19:41:35 -0800

    Added new file for organization

---
+++ JAD Final Project/WindowVar.java
@@ -0,0 +1,10 @@
+/**
+ * This is where we will move a lot of variables, as the file that holds all of
+ * them right now is starting to become bloated. This should also help with
+ * organization as now we will be able to put in any variables in here as we
+ * want and have much better organization
+ */
+
+public class WindowVar {
+
+}
