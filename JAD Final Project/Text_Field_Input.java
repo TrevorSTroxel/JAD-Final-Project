701ad6e4110e25be66e37c4feab195fd220ec4e9
TrevorSTroxel <ttroxel21@my.whitworth.edu>
2021-01-22 14:34:40 -0800

    Tested new JLabes to see how they looked

--- JAD Final Project/Text_Field_Input.java
+++ JAD Final Project/Text_Field_Input.java
@@ -35,18 +35,20 @@ public class Text_Field_Input {
 
     // creats a new file in the desired location
     public static void addPanel2() {
-        WindowVar.Panel2.setLayout(new GridLayout(3, 2));
+        WindowVar.Panel2.setLayout(new GridLayout(3, 3));
 
-        WindowVar.Panel2.add(WindowVar.P2_Enter_Get_Dir);
+        WindowVar.Panel2.add(WindowVar.P2_Enter_Get_Dir_Button);
+        WindowVar.Panel2.add(WindowVar.P2_Dir_Text_Label);
         WindowVar.Panel2.add(WindowVar.P2_Dir_Text_Field);
 
-        WindowVar.Panel2.add(WindowVar.P2_Enter_Get_File_Name);
-        WindowVar.Panel2.add(WindowVar.P2_File_Name);
+        WindowVar.Panel2.add(WindowVar.P2_Enter_Get_File_Name_Button);
+        WindowVar.Panel2.add(WindowVar.P2_File_Text_Label);
+        WindowVar.Panel2.add(WindowVar.P2_File_Name_Text_Field);
 
-        WindowVar.Panel2.add(WindowVar.P2_Enter_Call_Method);
+        WindowVar.Panel2.add(WindowVar.P2_Enter_Call_Method_Button);
 
         Text_Box_Settings(WindowVar.P2_Dir_Text_Field);
-        Text_Box_Settings(WindowVar.P2_File_Name);
+        Text_Box_Settings(WindowVar.P2_File_Name_Text_Field);
 
         WindowVar.Panel2.setVisible(true);
 
@@ -58,16 +60,16 @@ public class Text_Field_Input {
     public static void addPanel3() {
         WindowVar.Panel3.setLayout(new GridLayout(3, 2));
 
-        WindowVar.Panel3.add(WindowVar.P3_Enter_Get_Dir);
-        WindowVar.Panel3.add(WindowVar.P3_File_Path);
+        WindowVar.Panel3.add(WindowVar.P3_Enter_Get_Dir_Button);
+        WindowVar.Panel3.add(WindowVar.P3_File_Path_Text_Field);
 
-        WindowVar.Panel3.add(WindowVar.P3_Enter_Get_Class_Name);
-        WindowVar.Panel3.add(WindowVar.P3_Class_Name);
+        WindowVar.Panel3.add(WindowVar.P3_Enter_Get_Class_Name_Button);
+        WindowVar.Panel3.add(WindowVar.P3_Class_Name_Text_Field);
 
-        WindowVar.Panel3.add(WindowVar.P3_Enter_Call_Method);
+        WindowVar.Panel3.add(WindowVar.P3_Enter_Call_Method_Button);
 
-        Text_Box_Settings(WindowVar.P3_File_Path);
-        Text_Box_Settings(WindowVar.P3_Class_Name);
+        Text_Box_Settings(WindowVar.P3_File_Path_Text_Field);
+        Text_Box_Settings(WindowVar.P3_Class_Name_Text_Field);
 
         WindowVar.Panel3.setVisible(true);
 
@@ -78,20 +80,20 @@ public class Text_Field_Input {
     // Allows the user to make a class inside the file they made
     public static void addPanel4() {
         WindowVar.Panel4.setLayout(new GridLayout(4, 2));
-        WindowVar.Panel4.add(WindowVar.P4_Enter_File_Dir);
-        WindowVar.Panel4.add(WindowVar.P4_File_Path);
+        WindowVar.Panel4.add(WindowVar.P4_Enter_File_Dir_Button);
+        WindowVar.Panel4.add(WindowVar.P4_File_Path_Text_Field);
 
-        WindowVar.Panel4.add(WindowVar.P4_Enter_Return_Type);
-        WindowVar.Panel4.add(WindowVar.P4_Return_Type);
+        WindowVar.Panel4.add(WindowVar.P4_Enter_Return_Type_Button);
+        WindowVar.Panel4.add(WindowVar.P4_Return_Type_Text_Field);
 
-        WindowVar.Panel4.add(WindowVar.P4_Enter_Method_Name);
-        WindowVar.Panel4.add(WindowVar.P4_Method_Name);
+        WindowVar.Panel4.add(WindowVar.P4_Enter_Method_Name_Button);
+        WindowVar.Panel4.add(WindowVar.P4_Method_Name_Text_Field);
 
-        WindowVar.Panel4.add(WindowVar.P4_Enter_Call_Method);
+        WindowVar.Panel4.add(WindowVar.P4_Enter_Call_Method_Button);
 
-        Text_Box_Settings(WindowVar.P4_File_Path);
-        Text_Box_Settings(WindowVar.P4_Method_Name);
-        Text_Box_Settings(WindowVar.P4_Return_Type);
+        Text_Box_Settings(WindowVar.P4_File_Path_Text_Field);
+        Text_Box_Settings(WindowVar.P4_Method_Name_Text_Field);
+        Text_Box_Settings(WindowVar.P4_Return_Type_Text_Field);
 
         WindowVar.Panel4.setVisible(true);
 
@@ -102,20 +104,20 @@ public class Text_Field_Input {
     // allows the user to add contents to methods they have created
     public static void addPanel5() {
         WindowVar.Panel5.setLayout(new GridLayout(4, 2));
-        WindowVar.Panel5.add(WindowVar.P5_Enter_File_Dir);
-        WindowVar.Panel5.add(WindowVar.P5_File_Dir);
+        WindowVar.Panel5.add(WindowVar.P5_Enter_File_Dir_Button);
+        WindowVar.Panel5.add(WindowVar.P5_File_Dir_Text_Field);
 
-        WindowVar.Panel5.add(WindowVar.P5_Enter_Method_Name);
-        WindowVar.Panel5.add(WindowVar.P5_Method_Name);
+        WindowVar.Panel5.add(WindowVar.P5_Enter_Method_Name_Button);
+        WindowVar.Panel5.add(WindowVar.P5_Method_Name_Text_Field);
 
-        WindowVar.Panel5.add(WindowVar.P5_Enter_Contents);
-        WindowVar.Panel5.add(WindowVar.P5_Contents_To_Add);
+        WindowVar.Panel5.add(WindowVar.P5_Enter_Contents_Button);
+        WindowVar.Panel5.add(WindowVar.P5_Contents_To_Add_Text_Field);
 
-        WindowVar.Panel5.add(WindowVar.P5_Enter_Call_Method);
+        WindowVar.Panel5.add(WindowVar.P5_Enter_Call_Method_Button);
 
-        Text_Box_Settings(WindowVar.P5_File_Dir);
-        Text_Box_Settings(WindowVar.P5_Method_Name);
-        Text_Box_Settings(WindowVar.P5_Contents_To_Add);
+        Text_Box_Settings(WindowVar.P5_File_Dir_Text_Field);
+        Text_Box_Settings(WindowVar.P5_Method_Name_Text_Field);
+        Text_Box_Settings(WindowVar.P5_Contents_To_Add_Text_Field);
 
         WindowVar.Panel5.setVisible(true);
 
