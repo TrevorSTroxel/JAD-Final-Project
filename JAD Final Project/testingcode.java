3260bdb2196f0b81c57b68f7566c2910b8ab3ac5
TrevorSTroxel <ttroxel21@my.whitworth.edu>
2021-01-21 19:14:37 -0800

    trying out code that I found

--- JAD Final Project/testingcode.java
+++ JAD Final Project/testingcode.java
@@ -1,22 +1,7 @@
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-
 public class testingcode {
-    public static void main(String[] args) throws IOException {
-
-        List<String> fileContent = new ArrayList<>(Files
-                .readAllLines(Paths.get("C:/Users/ttroxel21/Desktop/New folder/test2.txt"), StandardCharsets.UTF_8));
-
-        for (int i = 0; i < fileContent.size(); i++) {
-            if (fileContent.get(i).equals("old line")) {
-                fileContent.set(i, "new line");
-
-            }
-        }
-        Files.write(Paths.get("C:/Users/ttroxel21/Desktop/New folder/test2.txt"), fileContent, StandardCharsets.UTF_8);
+    public static void main(String args[]) {
+        String str = "This(string)contains^special*characters&.";
+        str = str.replaceAll("[^a-zA-Z0-9]", " ");
+        System.out.println(str);
     }
 }
